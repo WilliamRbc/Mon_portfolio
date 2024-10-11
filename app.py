8770606c3addb1adaaf5a1ba63183db34c698e09
WilliamRbc <72968870+WilliamRbc@users.noreply.github.com>
2024-10-11 11:55:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,6 @@ def creer_graphique_secteurs_duree(df):
 
 
 # Initialisation de l'application Dash
-server = Flask(__name__)  # L'objet Flask
 app = Dash(__name__, external_stylesheets=["https://use.fontawesome.com/releases/v5.15.4/css/all.css"])
 app = app.server
 
