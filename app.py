54370c9738ece45d4339aaddcbac3724b288ac9e
WilliamRbc <72968870+WilliamRbc@users.noreply.github.com>
2024-10-11 10:19:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from dash import callback_context
 def load_runkeeper_data():
     df = pd.read_csv(r'01-runkeeper-data-export-79592130-2024-09-24-083524/cardioActivities.csv', on_bad_lines='skip', encoding='utf-8')
     df.columns = df.columns.str.strip().str.lower()
-    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y %H:%M')
+    df['Date'] = pd.to_datetime(df['ate'], format='%d/%m/%Y %H:%M')
     df['Week'] = df['Date'].dt.strftime('%U')
     df['Year'] = df['Date'].dt.strftime('%Y')
     return df[df['year'] == '2024']  # Filtrer uniquement les données de 2024
